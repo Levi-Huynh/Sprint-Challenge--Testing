eab3dc3faa24daacd7e0e6c500b2266d3534c8fa
Levi-Huynh <Levi_huynh@yahoo.com>
2019-07-01 15:08:55 -0700

    games/id and games/delete route

--- api/server.js
+++ api/server.js
@@ -21,6 +21,37 @@ Games.getAll()
 });
 
 
+server.get('/games/:id', async (req, res) => {
+  try {
+    const games = await Games.findById(req.params.id);
+    if (games) {
+      res.status(200).json(games);
+    } else {
+      res.status(404).json({ message: 'We could not find the games' });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'We ran into an error retrieving the ganes' });
+  }
+});
+
+server.delete('/:id', async (req, res) => {
+  try {
+    const count = await Games.remove(req.params.id);
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({
+        message: 'That game does not exist, perhaps it was deleted already',
+      });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'We ran into an error removing the game' });
+  }
+});
 
 server.post('/games', async (req, res) => {
     const game= req.body;
